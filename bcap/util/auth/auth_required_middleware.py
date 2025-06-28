caef1f1d59352a013cf7e0906832e9afa094a2c1
brett <brett@qedsystems.ca>
2025-06-28 06:37:56 -0700

    First cut at oauth auto-refresh

--- bcap/util/auth/auth_required_middleware.py
+++ bcap/util/auth/auth_required_middleware.py
@@ -4,23 +4,20 @@ from arches.app.models.system_settings import settings
 import re
 
 
-class AuthRequiredMiddleware(AuthenticationMiddleware):
-    # Redirect any unauthenticated users to home page
-    _valid_urls = []
-
-    def _clean_path(self, path):
+def should_bypass_auth(request):
+    def _clean_path(path):
         return re.sub(r"^/*", "", re.sub("/*$", "", path))
 
-    def _get_base_url(self):
-        return self._clean_path(
+    def _get_base_url():
+        return _clean_path(
             settings.FORCE_SCRIPT_NAME
             if settings.FORCE_SCRIPT_NAME
             else settings.BCGOV_PROXY_PREFIX
         )
 
-    def _get_public_urls(self):
+    def _get_public_urls():
         if len(AuthRequiredMiddleware._valid_urls) == 0:
-            base_url = self._get_base_url()
+            base_url = _get_base_url()
             for suffix in [
                 "",
                 "/auth",
@@ -31,25 +28,27 @@ class AuthRequiredMiddleware(AuthenticationMiddleware):
                 AuthRequiredMiddleware._valid_urls.append("%s%s" % (base_url, suffix))
         return AuthRequiredMiddleware._valid_urls
 
-    def _is_public_path(self, path):
-        cleaned_path = self._clean_path(path)
-        return cleaned_path in self._get_public_urls()
+    def _is_public_path(path):
+        cleaned_path = _clean_path(path)
+        return cleaned_path in _get_public_urls()
 
-    def bypass_auth(self, request):
-        request_source = (
-            request.META.get("REMOTE_ADDR")
-            if request.META.get("HTTP_X_FORWARDED_FOR") is None
-            else request.META.get("HTTP_X_FORWARDED_FOR")
-        )  # return True
-        return (
-            self._is_public_path(request.path)
-            or request_source in settings.AUTH_BYPASS_HOSTS
-            and request.META.get("HTTP_USER_AGENT").startswith("node-fetch/1.0")
-        )
+    request_source = (
+        request.META.get("REMOTE_ADDR")
+        if request.META.get("HTTP_X_FORWARDED_FOR") is None
+        else request.META.get("HTTP_X_FORWARDED_FOR")
+    )  # return True
+    return (
+        _is_public_path(request.path)
+        or request_source in settings.AUTH_BYPASS_HOSTS
+        and request.META.get("HTTP_USER_AGENT").startswith("node-fetch/1.0")
+    )
+
+
+class AuthRequiredMiddleware(AuthenticationMiddleware):
 
     def process_request(self, request):
         if not request.user.is_authenticated:
-            if not self.bypass_auth(request):
+            if not should_bypass_auth(request):
                 return HttpResponseRedirect("/%s/" % self._get_base_url())
             else:
                 print("Bypassing auth")
---
+++ bcap/util/auth/oauth_token_middleware.py
@@ -0,0 +1,74 @@
+import time
+from django.shortcuts import redirect
+from django.contrib.auth.models import AnonymousUser
+from requests_oauthlib import OAuth2Session
+from arches.app.utils.external_oauth_backend import ExternalOauthAuthenticationBackend
+from bcap.util.auth.auth_required_middleware import should_bypass_auth
+
+
+class OAuthTokenMiddleware:
+    UNAUTHENTICATED_REDIRECT = "http://localhost:82/bcap"
+
+    def __init__(
+        self,
+        get_response,
+        # client_id="your-client-id",
+        # client_secret="your-client-secret",
+        # token_url="https://your-oauth-server.com/token",
+    ):
+        self.get_response = get_response
+        # self.client_id = client_id
+        # self.client_secret = client_secret
+        # self.token_url = token_url
+
+    def __call__(self, request):
+        # for key, value in request.session.items():
+        #     print(f"{key}: {value}")
+        # print("\n\n")
+
+        if should_bypass_auth(request):
+            return self.get_response(request)
+
+        user = getattr(request, "user", None)
+
+        if not user or isinstance(user, AnonymousUser) or not user.is_authenticated:
+            # print("No user")
+            return redirect(OAuthTokenMiddleware.UNAUTHENTICATED_REDIRECT)
+
+        token_record = ExternalOauthAuthenticationBackend.get_token(request.user)
+
+        if not token_record:
+            # print("No token")
+            return redirect(OAuthTokenMiddleware.UNAUTHENTICATED_REDIRECT)
+
+        def update_token_record(new_token):
+            # print("\n\n\nUpdating token record\n\n\n")
+            token_record.access_token = new_token["access_token"]
+            token_record.access_token_expiration = new_token["expires_at"]
+            if "refresh_token" in new_token:
+                token_record.token_record.refresh_token = new_token["refresh_token"]
+            token_record.save()
+
+        current_time = time.time()
+        oauth2_settings = ExternalOauthAuthenticationBackend.get_oauth2_settings()
+        token = {
+            "access_token": token_record.access_token,
+            "refresh_token": token_record.refresh_token,
+            "token_type": "Bearer",
+            "expires_in": token_record.access_token_expiration.timestamp()
+            - current_time,
+        }
+
+        oauth = OAuth2Session(
+            client_id=oauth2_settings["app_id"],
+            token=token,
+            auto_refresh_url=oauth2_settings["token_endpoint"],
+            auto_refresh_kwargs={
+                "client_id": oauth2_settings["app_id"],
+                "client_secret": oauth2_settings["app_secret"],
+            },
+            token_updater=update_token_record,
+        )
+
+        request.oauth_session = oauth
+        return self.get_response(request)
