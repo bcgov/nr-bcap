bcgov/arches_common

214 - Update to use Arches Core 8.0.0a4, Docker Containers: add component-lab to docker containers, don't build webpack container by default, install vim by default run prettier on all javascript files

bcgov_arches_common/media/js/reports/default.js
@@ -1,14 +1,12 @@
-define([
-    'knockout', 
-    'viewmodels/report', 
-    'templates/views/report-templates/default.htm'
-], function(ko, ReportViewModel, defaultReportTemplate) {
-    return ko.components.register('default-report', {
-        viewModel: function(params) {
-            params.configKeys = [];
+import ko from "knockout";
+import ReportViewModel from "viewmodels/report";
+import defaultReportTemplate from "templates/views/report-templates/default.htm";
 
-            ReportViewModel.apply(this, [params]);
-        },
-        template: defaultReportTemplate
-    });
+export default ko.components.register("default-report", {
+    viewModel: function (params) {
+        params.configKeys = [];
+
+        ReportViewModel.apply(this, [params]);
+    },
+    template: defaultReportTemplate,
 });

bcgov/nr-bcap

bcap/media/js/reports/default.js
@@ -1,16 +1,11 @@
-define([
-    'underscore',
-        'knockout',
-        'viewmodels/bcap-site',
-        'templates/views/report-templates/bcap_default.htm',
-], function (_, ko, ReportViewModel, defaultTemplate) {
-    const viewModel = function(params) {
+import ko from "knockout";
+import ReportViewModel from "viewmodels/bcap-site";
+import defaultTemplate from "templates/views/report-templates/bcap_default.htm";
+
+export default ko.components.register("default-report", {
+    viewModel: function (params) {
         params.configKeys = [];
         ReportViewModel.apply(this, [params]);
-    };
-
-    return ko.components.register('default-report', {
-        viewModel: viewModel,
-        template: defaultTemplate
-    });
+    },
+    template: defaultTemplate,
 });