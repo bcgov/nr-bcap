f854be863aaefa732be886c7dfe300d6543467d5
brett <brett@qedsystems.ca>
2025-08-13 10:41:57 -0700

    Add missing shims

--- vite-shims/bootstrap-empty-export.js
+++
@@ -1,3 +0,0 @@
-// load Bootstrap 3 onto jQuery (expects global $)
-import 'bootstrap/dist/js/bootstrap.min.js';
-export default {}; // satisfy "import x from 'bootstrap'"
--- vite-shims/dropzone-default.js
+++
@@ -1,3 +0,0 @@
-// use the AMD build Arches maps to
-import * as DZ from 'dropzone/dist/min/dropzone-amd-module.min.js';
-export default DZ;
