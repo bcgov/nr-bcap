2c90dd7fe5364d58eaf431431e2aebc0ef430386
brett <brett@qedsystems.ca>
2025-08-20 08:22:50 -0700

    Delete branches to fix missing slug bug in v8, remove celery config in GH CI settings, fix middleware config

--- .github/github_settings.py
+++ .github/github_settings.py
@@ -241,7 +241,7 @@ MIDDLEWARE = [
     "arches.app.utils.middleware.ModifyAuthorizationHeader",
     "oauth2_provider.middleware.OAuth2TokenMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "bcap.util.auth.oauth_token_refresh.OAuthTokenRefreshMiddleware",
+    "bcgov_arches_common.util.auth.oauth_token_refresh.OAuthTokenRefreshMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "arches.app.utils.middleware.SetAnonymousUser",
@@ -444,9 +444,8 @@ CELERY_WORKER_NAME = get_env_variable("CELERY_WORKER_NAME", default="bcap_dev")
 
 # RabbitMQ --> "amqp://guest:guest@localhost",
 # Redis --> "redis://localhost:6379/0"
-CELERY_BROKER_URL = get_env_variable(
-    "CELERY_BROKER_URL", default="redis://localhost:6379/0"
-)
+CELERY_BROKER_URL = ""
+
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_RESULT_BACKEND = (
     "django-db"  # Use 'django-cache' if you want to use your cache as your backend
