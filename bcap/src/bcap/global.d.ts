37b6c037326edf6124441c615f88aa23fe8f390b
Brayden Carlson <braydenc@stratusadv.com>
2025-09-25 12:15:00 -0600

    Update global.d.ts

--- bcap/src/bcap/global.d.ts
+++ bcap/src/bcap/global.d.ts
@@ -3,16 +3,3 @@ import('@/arches/declarations.d.ts');
 declare module 'underscore';
 declare module 'arches';
 declare module 'utils/map-popup-provider';
-
-declare module '@/bcgov_arches_common/datatypes/geojson-feature-collection/types.ts' {
-    export interface AliasedGeojsonFeatureCollectionNode {
-        node_value: unknown;
-        display_value: string;
-        details: unknown[];
-    }
-}
-
-declare module '@/bcgov_arches_common/components/SimpleMap/SimpleMap.vue' {
-    const component: unknown;
-    export default component;
-}
