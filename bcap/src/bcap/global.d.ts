9719bf0a71406367f81cc6cb929f740fe6c4e256
brett <brett@qedsystems.ca>
2025-10-02 20:35:27 -0700

    Trying to fix TS error

--- bcap/src/bcap/global.d.ts
+++ bcap/src/bcap/global.d.ts
@@ -3,3 +3,6 @@ import('@/arches/declarations.d.ts');
 declare module 'underscore';
 declare module 'arches';
 declare module 'utils/map-popup-provider';
+
+// bcgov-arches-common modules
+declare module 'utils/map-filter-utils';
