c3c53b62d14851d5e9944344403c347e56f236e2
brett <brett@qedsystems.ca>
2025-12-10 13:10:50 -0800

    Remove restricted site function, update aliases from database

--- bcap/util/aliases/contributor.py
+++ bcap/util/aliases/contributor.py
@@ -2,16 +2,16 @@ from bcap.util.bcap_aliases import AbstractAliases
 
 
 class ContributorAliases(AbstractAliases):
-    ASSOCIATED_ORGANIZATION = 'associated_organization'
-    CONTACT_EMAIL = 'contact_email'
-    CONTACT_PHONE_NUMBER = 'contact_phone_number'
-    CONTRIBUTOR_NAME = 'contributor_name'
-    CONTRIBUTOR_ROLE = 'contributor_role'
-    CONTRIBUTOR_TYPE = 'contributor_type'
-    END_DATE = 'end_date'
-    FIRST_NAME = 'first_name'
-    INACTIVE = 'inactive'
-    START_DATE = 'start_date'
+    ASSOCIATED_ORGANIZATION = "associated_organization"
+    CONTACT_EMAIL = "contact_email"
+    CONTACT_PHONE_NUMBER = "contact_phone_number"
+    CONTRIBUTOR_NAME = "contributor_name"
+    CONTRIBUTOR_ROLE = "contributor_role"
+    CONTRIBUTOR_TYPE = "contributor_type"
+    END_DATE = "end_date"
+    FIRST_NAME = "first_name"
+    INACTIVE = "inactive"
+    START_DATE = "start_date"
 
     @staticmethod
     def get_aliases():
