1f506591728cb5e084e7ba927d35670e75ab558c
brett <brett@qedsystems.ca>
2026-02-23 15:07:48 -0800

    Fix black format

--- bcap/sql_config.py
+++ bcap/sql_config.py
@@ -6,25 +6,33 @@ from bcap.migrations.util.migration_util import format_sql
 sql_items = [
     SQLItem(
         "bc_labelled_geojson_geometries",
-        format_sql("sql/views/bc_labelled_geojson_geometries.sql", ),
+        format_sql(
+            "sql/views/bc_labelled_geojson_geometries.sql",
+        ),
         reverse_sql="drop view bc_labelled_geojson_geometries;",
         replace=True,
     ),
     SQLItem(
         "bc_labelled_site_geometries",
-        format_sql("sql/views/bc_labelled_site_geometries.sql", ),
+        format_sql(
+            "sql/views/bc_labelled_site_geometries.sql",
+        ),
         reverse_sql="drop view bc_labelled_site_geometries;",
         replace=True,
     ),
     SQLItem(
         "bc_labelled_site_visit_geometries",
-        format_sql("sql/views/bc_labelled_site_visit_geometries.sql", ),
+        format_sql(
+            "sql/views/bc_labelled_site_visit_geometries.sql",
+        ),
         reverse_sql="drop view bc_labelled_site_visit_geometries;",
         replace=True,
     ),
     SQLItem(
         "bc_labelled_sandcastle_geometries",
-        format_sql("sql/views/bc_labelled_sandcastle_geometries.sql", ),
+        format_sql(
+            "sql/views/bc_labelled_sandcastle_geometries.sql",
+        ),
         reverse_sql="drop view bc_labelled_sandcastle_geometries;",
         replace=True,
     ),
