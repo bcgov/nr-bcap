58287dbc8f82894bc4f8344bd0aab56654476b1b
Philip Hansen <philip.pt.hansen@gmail.com>
2026-04-10 15:24:57 -0600

    step1 Acceptance Criteria

--- bcap/src/bcap/apps/Permit/SubmitApplication/steps/Step1_About.vue
+++ bcap/src/bcap/apps/Permit/SubmitApplication/steps/Step1_About.vue
@@ -7,49 +7,99 @@ const isValid = () => {
 defineExpose({ isValid });
 </script>
 <template>
-    <FieldSet legend="Before you begin">
-        <div class="mb-2">
-            <p>Stuff you need to know</p>
-        </div>
-
-        <ul class="bullet-list ml-4">
-            <li>Point 1</li>
-            <li>Point 2</li>
-            <li>Point 3</li>
-        </ul>
-
-        <h3 class="mb-2 font-bold mt-4">Submission Requirements</h3>
-
+    <FieldSet>
+        <h3>Permit Deliverables</h3>
+        <br />
         <ul class="bullet-list ml-4">
             <li>
-                Steps 2-5 and 10 are
-                <i>something</i>
-                to complete the basic data requirements for a notification.
+                Permit deliverables are expected to comply with Branch
+                standards, including Archaeological Impact Assessment
+                Guidelines, the Interim Reporting Policy, and
+            </li>
+            <li>Site Form, Mapping, and Spatial Requirements.</li>
+            <li>Permit deliverables submitted to the Branch include:</li>
+        </ul>
+        <br />
+        <ol>
+            <li>
+                Site records (sent to
+                <a
+                    class="emailLink"
+                    href="mailto:archsiteform@gov.bc.ca"
+                >
+                    archsiteform@gov.bc.ca
+                </a>
+                ) a. Artifact Catalogues, Faunal Analysis Reports, & Dating Lab
+                Reports may be submitted at a later date, but no later than the
+                deliverables due date noted in the permit unless otherwise
+                approved by the Branch. PDF of Final Report uploaded via the
+                Branch’s permit application system, Archaeology Permit Tracking
+                System (APTS) Hardcopies of reports will be submitted to the
+                Branch on request. When applicable, interims, specialized
+                analyses (e.g., radiocarbon dating), and Notice of Intent
+                (“NoI”) summaries (as defined in the Multi-Assessment Permit
+                Policy) will be appended to reports
             </li>
             <li>
-                Information required for Steps 2-5:
-                <ul class="bullet-list ml-4 mt-2">
-                    <li>First thing</li>
-                    <li>Second thing</li>
+                Spatial files for study areas, Subsurface Test Areas (“STAs”),
+                and Areas of Potential (“AOPs”) (sent to
+                <a
+                    class="emailLink"
+                    href="mailto:archpermitspatial@gov.bc.ca"
+                >
+                    archpermitspatial@gov.bc.ca
+                </a>
+                )
+            </li>
+            <li>
+                Confirmation of acceptance from the repository uploaded via the
+                Branch’s permit application system.
+            </li>
+            <li>Grant of license.</li>
+            <li>
+                The permit holder will provide First Nations with reports. Other
+                data (e.g., spatial files; site forms; photographs) will be
+                provided upon request.
+                <ul class="bullet-list ml-4">
                     <li>
-                        Third things:
-                        <ul class="bullet-list-nested ml-4 mt-1">
-                            <li>One</li>
-                            <li>Two</li>
-                        </ul>
+                        The permit report and site form(s) will identify the
+                        repository.
+                    </li>
+                    <li>
+                        Site information described in reports must precisely
+                        match provincial site records (e.g., boundaries,
+                        recovered materials).
                     </li>
                 </ul>
             </li>
-        </ul>
-        <div class="mt-4">
-            <span class="red">*</span>
-            The system does not have a save for later or draft function, so it
-            is important to have all information ready before starting. It is
-            highly recommended to complete the remaining steps if information is
-            available to improve the historic site’s search ability on the
-            register.
-        </div>
+            <li>
+                a. b. 24 S. 12.2 Heritage Inspection Permit – Terms and
+                Conditions Last Updated: April 11, 2025
+                <ul class="bullet-list ml-4">
+                    <li>
+                        When there is a discrepancy between the site record
+                        following fieldwork and what is available via Remote
+                        Access to Archaeological Data (“RAAD”) the approach to
+                        resolve the discrepancy will be confirmed by the Branch.
+                    </li>
+                    <li>
+                        Photographs of diagnostic artifacts, formed tools, and
+                        rock art must be included in the site record.
+                    </li>
+                    <li>
+                        With large sample sizes, a permit report may include
+                        only a selection of photographs.
+                    </li>
+                    <li>
+                        The site record and report may be redacted prior to
+                        distribution via the RAAD and the Provincial
+                        Archaeologica
+                    </li>
+                </ul>
+            </li>
+        </ol>
     </FieldSet>
+    <br />
 </template>
 
 <style>
@@ -69,5 +119,10 @@ li {
     list-style-type: circle;
     padding-left: 2rem;
     margin-top: 0.25rem;
+}
+
+.emailLink {
+    color: #0000ee;
+    text-decoration: underline;
 }
 </style>
