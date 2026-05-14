05851e7b22b270ec5764a3646312f6b2e3af07d3
Travis Semple <travis8814@gmail.com>
2026-05-14 10:52:53 -0700

    prettier

--- bcap/src/bcap/components/pages/details/ArchaeologicalSite/sections/DetailsSection1.test.ts
+++ bcap/src/bcap/components/pages/details/ArchaeologicalSite/sections/DetailsSection1.test.ts
@@ -1,22 +1,25 @@
 import { describe, it, expect, vi } from 'vitest';
 import { mount } from '@vue/test-utils';
 
-vi.mock('@/bcgov_arches_common/components/SimpleMap/SimpleMap.vue', async () => {
-    const { defineComponent } = await import('vue');
-    return {
-        default: defineComponent({
-            name: 'MapStub',
-            props: {
-                graphSlug: String,
-                nodeAlias: String,
-                mode: String,
-                aliasedNodeData: { type: Object, default: undefined },
-                useUtmCoords: { type: Boolean, default: false },
-            },
-            template: '<div data-testid="map-stub"></div>',
-        }),
-    };
-});
+vi.mock(
+    '@/bcgov_arches_common/components/SimpleMap/SimpleMap.vue',
+    async () => {
+        const { defineComponent } = await import('vue');
+        return {
+            default: defineComponent({
+                name: 'MapStub',
+                props: {
+                    graphSlug: String,
+                    nodeAlias: String,
+                    mode: String,
+                    aliasedNodeData: { type: Object, default: undefined },
+                    useUtmCoords: { type: Boolean, default: false },
+                },
+                template: '<div data-testid="map-stub"></div>',
+            }),
+        };
+    },
+);
 
 vi.mock('@/arches_component_lab/widgets/constants.ts', () => ({
     VIEW: 'view',
