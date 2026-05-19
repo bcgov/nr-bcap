6f33c6202e0d5669a984eb1dd4580cef21338a06
Brett Ferguson <brett@qedsystems.ca>
2026-05-19 15:04:07 -0700

    Brf/add missing permit nodes (#1485)
    * set to uneditable for Borden Number Issuance Date (#1473)
    
    * Brf/feat/1271 add publication resource model (#1458)
    
    * 1271 -Add initial cut of publication resource model
    
    * 1271 - Add initial cut of publication resource model
    
    * 1271 - Commit outcome of ETL concept->reference updates
    
    * 1271 - Add publication to Arch Site resource model. Start of schema, details page update
    
    * 1271 - Move reference details to publication resource model, fix publication authors, update site details page to pull reference details from publication resource model
    
    * Add remarks to publication model and remove individual publication fields from arch site,
    Add placeholder nodes to permit application
    
    * Pull latest resource model definitions
    
    * Commit outcome of ETL concept->reference updates, clear source branch publication id
    
    * Pull publication_reference up to grouping node, remove individual publication fields from site visit
    
    * Fix column header, work on TS errors
    
    * Fix typo
    
    * Fix Archaeological Site map styling method, add in Site Visit for MVT_tiler (#1455)
    
    * Add in site visit for MVT_tiler, add in patterns files
    
    * Frontend fixes and clean up
    
    * Add in migration that should set the colors correctly depending on their status
    
    * small warning fixes
    
    * change registration status to left join, readd styleimagemissing event
    
    * fix graph update in migration
    
    * Add in pattern markers by using MapMarkers, update Archaeological Site.json, remove mapbox-gl.js
    
    * Add clustering styling back, remove styling from migration to JSON only
    
    * UTM Coordinates to Site Map (#1466)
    
    * add in prop use-utm-coords=true
    
    * add another unit test just to increase coverage
    
    * another unit test
    
    * add in unit test for DetailsSection1.test.ts
    
    * prettier
    
    * Combine General Remarks for Arch Visit and Site Visit(s) on Details Section 8 (#1468)
    
    * Combine arch site remarks and site visit remarks under DetailsSection8
    
    * add unit test to test data conversion
    
    * fix vitest resolver
    
    * comment
    
    * add in multiple remark unit test
    
    * Increment version to 2.0.0a7 (#1483)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Add missing permit/requirement nodes
    
    ---------
    
    Co-authored-by: Travis Semple <travis8814@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ bcap/migrations/sql/v100/v2026.05.11__get_map_attribute_data.sql
@@ -0,0 +1,67 @@
+drop function if exists get_map_attribute_data;
+create or replace function get_map_attribute_data(p_resourceinstanceid uuid, nodeid uuid) returns jsonb as
+    $$
+declare
+    l_arch_site_geom_node_id text = 'b18223c2-13ef-11f0-8695-0242ac170007';
+    l_arch_site_legislative_act_id text = '034d2e02-13f2-11f0-9ff8-0242ac170007';
+
+    l_borden_number_id text = '7e15332c-1c54-11f0-b5bf-0242ac170007';
+    l_borden_numer_nodegroup_id text = '034d1c32-13f2-11f0-9ff8-0242ac170007';
+
+    l_leg_act_authority_node_id text = '7789d580-3b87-11ee-a701-080027b7463b';
+
+    l_decision_date_node_id text = 'f80f0c00-1977-11f0-8713-0242ac170008';
+    l_registration_status_node_id text = '4abdfeea-8d15-4ea6-94bd-d2385d47a5ac';
+    l_registration_status_nodegroup_id text = 'f80f08ae-1977-11f0-8713-0242ac170008';
+
+    data jsonb;
+begin
+    if nodeid = l_arch_site_geom_node_id::uuid then -- Archaeological Site
+
+        with borden_number as (
+            select resourceinstanceid,
+                   tiledata ->> l_borden_number_id as borden_number
+            from tiles
+            where nodegroupid = l_borden_numer_nodegroup_id::uuid
+              and resourceinstanceid = p_resourceinstanceid
+        ),
+        arch_site_leg_acts as (
+            select t.resourceinstanceid,
+                   (jsonb_array_elements(tiledata -> l_arch_site_legislative_act_id) ->> 'resourceId')::uuid as legislative_act_id
+            from tiles t
+            where nodegroupid = '034d1fac-13f2-11f0-9ff8-0242ac170007'::uuid
+              and t.resourceinstanceid = p_resourceinstanceid
+              and tiledata -> l_arch_site_legislative_act_id is not null
+        ),
+        authorities as (
+            select resourceinstanceid,
+                   tiledata -> l_leg_act_authority_node_id -> 0 -> 'labels' -> 0 ->> 'value' as authority
+            from tiles
+            where nodegroupid = '7789d580-3b87-11ee-a701-080027b7463b'::uuid
+        ),
+        registration_status as (
+            select resourceinstanceid,
+                   tiledata->l_registration_status_node_id->0->'labels'->0->>'value' status
+            from tiles where
+                           resourceinstanceid = p_resourceinstanceid and
+                nodegroupid = l_registration_status_nodegroup_id::uuid
+                       order by tiledata->>l_decision_date_node_id desc limit 1
+        )
+        select jsonb_build_object(
+            'authorities', coalesce(array_agg(distinct a.authority) filter (where a.authority is not null), '{}'::text[]),
+            'borden_number', bn.borden_number,
+            'registration_status', rs.status
+        )
+        into data
+        from borden_number bn
+             left join arch_site_leg_acts hs on hs.resourceinstanceid = bn.resourceinstanceid
+             left join authorities a on a.resourceinstanceid = hs.legislative_act_id
+             left join registration_status rs on rs.resourceinstanceid = bn.resourceinstanceid 
+        where bn.resourceinstanceid = p_resourceinstanceid
+        group by bn.resourceinstanceid, bn.borden_number, rs.status;
+
+    end if;
+    return data;
+end;
+$$
+    language plpgsql;
