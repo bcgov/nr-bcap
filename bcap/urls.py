a458b99a4c9e9e5baaa6631f9ef06d7d112ff0d8
Travis Semple <travis8814@gmail.com>
2026-05-26 06:44:07 -0700

    unit test fix

--- bcap/urls.py
+++ bcap/urls.py
@@ -134,7 +134,7 @@ urlpatterns = [
     path(f"{PREFIX}search/export_results", export_results, name="export_results"),
     path(
         f"{PREFIX}api/schema",
-        SpectacularAPIView.as_view(),
+        SpectacularAPIView.as_view(urlconf="bcap.documented_api_urls"),
         name="schema",
     ),
     path(
