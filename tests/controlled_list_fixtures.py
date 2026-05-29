d3779fdcf446eeb438a8d12176da59a6ab08808e
Travis Semple <travis8814@gmail.com>
2026-05-29 07:56:07 -0700

    these are already loaded in via migration

--- tests/controlled_list_fixtures.py
+++ tests/controlled_list_fixtures.py
@@ -19,8 +19,6 @@ class ControlledListFixtures:
         ("contributor", "contributor_type", "Archaeologist", 1),
         ("contributor", "contributor_type", "Consultant", 2),
         ("hca_permit", "hca_permit_type", "Investigation", 1),
-        ("permit_application", "industrial_sector", "Forestry", 1),
-        ("permit_application", "industrial_sector", "Mining", 2),
     ]
 
     @classmethod
