b90848cf20e3e458eb0a4324d22bf2fbb4b390c0
brett <brett@qedsystems.ca>
2026-07-24 12:18:38 -0700

    Partial migration to use alias->uuid functions

--- bcap/migrations/sql/functions/get_map_attribute_data.sql
+++ bcap/migrations/sql/functions/get_map_attribute_data.sql
@@ -2,7 +2,6 @@ drop function if exists get_map_attribute_data;
 create or replace function get_map_attribute_data(p_resourceinstanceid uuid, nodeid uuid) returns jsonb as
     $$
 declare
-    l_arch_site_geom_node_id text = 'b18223c2-13ef-11f0-8695-0242ac170007';
     l_arch_site_legislative_act_id text = '034d2e02-13f2-11f0-9ff8-0242ac170007';
 
     l_borden_number_id text = '7e15332c-1c54-11f0-b5bf-0242ac170007';
@@ -16,8 +15,7 @@ declare
 
     data jsonb;
 begin
-    if nodeid = l_arch_site_geom_node_id::uuid then -- Archaeological Site
-
+    if nodeid = archaeological_site.node_alias_uuid('site_boundary') then -- Archaeological Site
         with borden_number as (
             select resourceinstanceid,
                    tiledata ->> l_borden_number_id as borden_number
@@ -27,17 +25,17 @@ begin
         ),
         arch_site_leg_acts as (
             select t.resourceinstanceid,
-                   (jsonb_array_elements(tiledata -> l_arch_site_legislative_act_id) ->> 'resourceId')::uuid as legislative_act_id
+                   (jsonb_array_elements(tiledata -> archaeological_site.node_alias_uuid('legislative_act')::text) ->> 'resourceId')::uuid as legislative_act_id
             from tiles t
-            where nodegroupid = '034d1fac-13f2-11f0-9ff8-0242ac170007'::uuid
+            where nodegroupid = archaeological_site.node_alias_uuid('authority')
               and t.resourceinstanceid = p_resourceinstanceid
               and tiledata -> l_arch_site_legislative_act_id is not null
         ),
         authorities as (
             select resourceinstanceid,
-                   tiledata -> l_leg_act_authority_node_id -> 0 -> 'labels' -> 0 ->> 'value' as authority
+                   tiledata -> legislative_act.node_alias_uuid('authority')::text -> 0 -> 'labels' -> 0 ->> 'value' as authority
             from tiles
-            where nodegroupid = '7789d580-3b87-11ee-a701-080027b7463b'::uuid
+            where nodegroupid = legislative_act.node_alias_uuid('authority')
         ),
         registration_status as (
             select resourceinstanceid,
