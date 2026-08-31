57db19fea3deff30093d63343d7698f774d6827d
Travis Semple <travis8814@gmail.com>
2026-08-31 14:01:06 -0700

    remove comment

--- bcap/services/virus_scan_service.py
+++ bcap/services/virus_scan_service.py
@@ -35,7 +35,6 @@ class VirusScanService:
             )
             status, reason = scanner.instream(file)["stream"]
         except clamd.BufferTooLongError:
-            # clamd refuses streams over StreamMaxLength (25M by default).
             logger.error("ClamAV rejected an oversized stream")
             return ["File is too large to virus scan"]
         except (clamd.ClamdError, OSError) as e:
