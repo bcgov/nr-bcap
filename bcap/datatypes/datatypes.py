c76ecee852b9427dee601ed8d71b30f438d13f72
Travis Semple <travis8814@gmail.com>
2026-09-11 09:43:10 -0700

    Add in ClamAV Virus Scanning for Backend Storage (#1705)
    * Add in ClamAV for Arches
    
    * move files
    
    * override queryset not core
    
    * use stream
    
    * clean up
    
    * lower
    
    * add a reasoning comment
    
    * add in warning for ClamAV disabled
    
    * move validate around a bit
    
    * always pull
    
    * clean up env
    
    * handle file too large
    
    * remove data type, return 400 instead of 500
    
    * already scanned file
    
    * put in exception handling later
    
    * change to TileValidationError which is a ValidationError, indicate which file has issues
    
    * add in config for larger uploads
    
    * remove comment
    
    * remove array
    
    * handle other scenario
    
    * fix lock
    
    * clean up
    
    * move comment closer
    
    * logging so it's consistent in each branch
    
    * Update docker-compose.yml
    
    * missing package-lock
    
    * fix package lock

--- bcap/datatypes/datatypes.py
+++
@@ -1,7 +0,0 @@
-# The datatype factory resolves the geojson row (modulename=datatypes.py,
-# classname=GeojsonFeatureCollectionDataType) by searching arches-app dirs
-# before core, so this re-export shadows the core class with the
-# arches_querysets per-tile version. arches_querysets never wires it up itself.
-from arches_querysets.datatypes.geojson import (
-    GeojsonFeatureCollectionDataType,
-)  # noqa: F401
