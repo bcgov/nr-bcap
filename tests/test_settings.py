cba2526341f0a1b0ac7b863d6500c7269ba11997
Travis Semple <travis8814@gmail.com>
2026-09-15 07:38:44 -0700

    remove warnings for django.request

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -69,6 +69,11 @@ LOGGING["loggers"]["bcap.util.storage_filename_generator"] = {
     "propagate": True,
 }
 
+LOGGING["loggers"]["django.request"] = {
+    "level": "ERROR",
+    "propagate": True,
+}
+
 ELASTICSEARCH_PREFIX = "test"
 
 TEST_RUNNER = "tests.runner.BcapTestRunner"
